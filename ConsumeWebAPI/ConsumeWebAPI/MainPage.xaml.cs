d7ea1404a73c27da7c4e2244e196dfda9e26a70f
Gaurav Arora <g_arora@hotmail.com>
2015-03-21 20:00:38 +0530

    added support of RestSharp and implemented Async method

--- ConsumeWebAPI/ConsumeWebAPI/MainPage.xaml.cs
+++ ConsumeWebAPI/ConsumeWebAPI/MainPage.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ConsumeWebAPI.Model;
 using Microsoft.Phone.Controls;
-using Newtonsoft.Json;
+using RestSharp;
 
 namespace ConsumeWebAPI
 {
@@ -17,64 +11,26 @@ namespace ConsumeWebAPI
         public MainPage()
         {
             InitializeComponent();
-            var data = GetServerData();
-            LstServerData.ItemsSource = new ServerDataViewModel().DisplayData(data.Result);
+
+            GetServerData();
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
-        private async Task<List<ServerData>> GetServerData()
+
+        private void GetServerData()
         {
-            var serverData = new List<ServerData>();
+            const string url = "http://crudwithwebapi.azurewebsites.net";
+            var client = new RestClient(url);
+            var request = new RestRequest("api/serverdata", Method.GET) { RequestFormat = DataFormat.Json };
 
-            using (var client = new HttpClient())
+            client.ExecuteAsync<List<ServerData>>(request, response =>
             {
-                client.BaseAddress = new Uri("http://crudwithwebapi.azurewebsites.net");
+                LstServerData.ItemsSource = response.Data;
+            });
 
-                const string url = "api/serverdata/1";
-
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                var response = await client.GetAsync(url);
-
-                if (!response.IsSuccessStatusCode) return serverData;
-
-                var data = response.Content.ReadAsStringAsync();
-                serverData = JsonConvert.DeserializeObject<List<ServerData>>(data.Result);
-            }
-
-            return serverData;
         }
-        //private List<ServerData> GetServerData()
-        //{
-        //    const string uri = "http://crudwithwebapi.azurewebsites.net/api/serverdata";
-        //    var serverData = new List<ServerData>();
-        //    var client = new WebClient();
-        //    client.Headers["Accept"] = "application/json";
-        //    client.DownloadStringAsync(new Uri(uri));
-        //    client.DownloadStringCompleted += (s1, e1) =>
-        //    {
-        //        serverData =
-        //            JsonConvert.DeserializeObject<List<ServerData>>(e1.Result.ToString(CultureInfo.InvariantCulture));
-        //    };
 
-        //    return serverData;
-        //}
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
--- ConsumeWebAPI/ConsumeWebAPI/Model/ServerData.cs
+++ ConsumeWebAPI/ConsumeWebAPI/Model/ServerData.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsumeWebAPI.Model
+﻿using System;
+
+namespace ConsumeWebAPI.Model
 {
     public class ServerData
     {
@@ -10,5 +12,20 @@
         public string IP { get; set; }
         public int Type { get; set; }
         public int RecordIdentifier { get; set; }
+
+        public String Title
+        {
+            get { return string.Format("Data History of ID:{0}", Id); }
+        }
+
+        public String Description
+        {
+            get
+            {
+                return string.Format("Start Date:{0}, End Date:{1}, Order Number:{2}, IP:{3}, Record Type:{4}",
+                    InitialDate, EndDate, OrderNumber, IP,
+                    Type);
+            }
+        }
     }
 }
